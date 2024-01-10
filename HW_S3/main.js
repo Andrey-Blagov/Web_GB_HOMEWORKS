f5ba870dbbc345149ae1a2cb87349e09f16c7a6e
Andrey <blagovandrey1323@gmail.com>
2024-01-10 14:15:37 +0300

    Add HW_S3

---
+++ HW_S3/main.js
@@ -0,0 +1,10 @@
+function greeting(nm){
+    alert(`Nice to meet you, ${n} !!!`);
+    
+    console.log(`Nice to meet you, ${n} !!!`);
+}
+
+const n = prompt('Hello! What is your name ?')
+
+greeting(n);
+
